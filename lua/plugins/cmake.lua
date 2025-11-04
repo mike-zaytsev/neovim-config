1fd6892dc10239a47a9a5655a75760f54cd25c38
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2025-11-04 20:28:32 +0300

    disable FetchContent silencing in cmake

--- lua/plugins/cmake.lua
+++ lua/plugins/cmake.lua
@@ -6,6 +6,7 @@ return {
         cmake_generate_options = {
             "-DCMAKE_EXPORT_COMPILE_COMMANDS=1",
             "-DCMAKE_INSTALL_PREFIX=./install",
+            "-DFETCHCONTENT_QUIET=OFF",
             "-GNinja",
         },
         cmake_build_directory = "build/${variant:buildType}",
