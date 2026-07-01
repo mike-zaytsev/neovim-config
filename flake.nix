d4dc2ffe79fb47885b9ca8189730e9d2e83cbc79
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2026-07-01 17:37:00 +0300

    add macos support

--- flake.nix
+++ flake.nix
@@ -14,6 +14,8 @@
       systems = [
         "x86_64-linux"
         "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
 
       forAllSystems = nixpkgs.lib.genAttrs systems;
